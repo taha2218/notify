e38858aa5c3a8435e5d580b0113be5ff23fbe7d4
taha2218 <ta.22.ha.18.s@gmail.com>
2020-09-27 02:30:55 +0530

    Design changes in bottom Navbar

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -15,8 +15,8 @@ class HomePage extends StatelessWidget {
 
   final List<Widget> _screens = [
     HomeScreen(),
-    ProfilePage(),
     ChannelsScreen(),
+    ProfilePage(),
     BookMarkScreen(),
     AboutUsScreen(),
   ];
@@ -41,10 +41,10 @@ class HomePage extends StatelessWidget {
       title: Text(
         "Notifier",
         textAlign: TextAlign.center,
-        style: GoogleFonts.ubuntu(textStyle: TextStyle(color: Colors.black.withOpacity(0.88),fontSize: 21,fontWeight: FontWeight.w600)),
+        style: GoogleFonts.ubuntu(textStyle: TextStyle(color: Colors.black.withOpacity(0.88),fontSize: 18,fontWeight: FontWeight.w500)),
       ),
       actions: [
-        IconButton(icon: Icon(Icons.exit_to_app), onPressed: (){Navigator.pop(context);})
+        IconButton(icon: Icon(Icons.exit_to_app,size: 22,), onPressed: (){Navigator.pop(context);})
       ],
     );
   }
@@ -59,36 +59,36 @@ class HomePage extends StatelessWidget {
     final _indexProvider = Provider.of<Index>(context);
     int _currentIndex = _indexProvider.indexOfScreen;
     return BottomNavigationBar(
-      showSelectedLabels: true,
+      showSelectedLabels: false,
       showUnselectedLabels: false,
       type: BottomNavigationBarType.fixed,
-      unselectedItemColor: Colors.black,
-      selectedItemColor: Colors.indigo,
+      unselectedItemColor: Colors.black.withOpacity(0.75),
+      selectedItemColor: Colors.indigo[800],
       currentIndex: _currentIndex,
       onTap: (value){
-        if (value == 1) Get.toNamed('profile');
+        if (value == 2) Get.toNamed('profile');
         else _indexProvider.indexOfScreen = value;
       },
       items: [
         BottomNavigationBarItem(
-          icon: Icon(Icons.home,),
-          title: Text("Home",style: TextStyle(color: Colors.black, fontSize: 11),)
+          icon: Icon(Icons.home, size: 27,),
+          title: Text("Home",style: TextStyle(color: Colors.black, fontSize: 11),),
         ),
         BottomNavigationBarItem(
-          icon: Icon(Icons.person,), 
-          title: Text("Profile",style: TextStyle(color: Colors.black, fontSize: 11),)
+          icon: Icon(Icons.view_headline, size: 27), 
+          title: Text("Channels",style: TextStyle(color: Colors.black, fontSize: 11),)
         ),
         BottomNavigationBarItem(
-          icon: Icon(Icons.view_list,),
-          title: Text("Channels",style: TextStyle(color: Colors.black, fontSize: 11),),
+          icon: Icon(Icons.person, size: 27),
+          title: Text("Profile",style: TextStyle(color: Colors.black, fontSize: 11),),
         ),
         BottomNavigationBarItem(
-          icon: Icon(Icons.bookmark_border,), 
-          title: Text('BookMark',style: TextStyle(color: Colors.black, fontSize: 11),)
+          icon: Icon(Icons.bookmark, size: 27), 
+          title: Text('BookMark',style: TextStyle(color: Colors.black, fontSize: 11),),
         ),
         BottomNavigationBarItem(
-          icon: Icon(Icons.info_outline,),
-          title: Text('About',style: TextStyle(color: Colors.black, fontSize: 11),))
+          icon: Icon(Icons.info, size: 27),
+          title: Text('About',style: TextStyle(color: Colors.black, fontSize: 11),)),
       ],
     );
   }
